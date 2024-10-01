d9eb1c9c1105d621df723f18e38ec5cee5ecbd00
WG32 TALDEA <ik012982i5@H012982.NET>
2024-10-01 13:56:42 +0200

    sortzen addlistenerrak

--- NanoClicker/js/script.js
+++ NanoClicker/js/script.js
@@ -1,9 +1,29 @@
 var kontagailua = new Kontagailua();
+var atzekoAleroia = document.getElementById("atzekoAleroia");
+var aurrekoAleroia = document.getElementById("aurrekoAleroia");
+var drs = document.getElementById("drs");
+var ruedas = document.getElementById("ruedas");
+var kotxea = document.getElementById("kotxea");
+var motor = document.getElementById("motorra");
 
 
 
-addEventListener('click', function() {
+kontagailua.addEventListener('click', function() {
     kontagailua.nanoClickGehitu();
 });
+
+atzekoAleroia.addEventListener('click', function(){
+    atzekoAleroia.gehituAbiadura();
+});
+
+aurrekoAleroia.addEventListener('click', function(){
+    aurrekoAleroia.gehituAbiadura();
+});
+
+drs.addEventListener('click', function(){
+    drs.gehituAbiadura();
+});
+
+
 
 
